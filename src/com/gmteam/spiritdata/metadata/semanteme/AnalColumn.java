7286bf98a5c8e72f3bbe0231dd6c4cc4a3afda43
wanghuiwhwx <wanghui_whwx@sina.com>
2014-10-27 11:52:21 +0800

    分析框架

--- src/com/gmteam/spiritdata/metadata/semanteme/AnalColumn.java
+++ src/com/gmteam/spiritdata/metadata/semanteme/AnalColumn.java
@@ -5,17 +5,17 @@ import java.util.Map;
 import com.gmteam.spiritdata.metadata.relation.pojo.MetadataModel;
 
 /**
- * 分析字段语义的方法
+ * 以字段为目标的语义分析方法
  * 所有字段语义分析方法都可继承此接口
  * @author wh
  */
-public interface AnalSemanteme {
+public interface AnalColumn {
     /**
-     * 根据表名，元数据信息，列名称分析元数据
+     * 扫描表，并分析某一列的语义
      * @param tableName 表名
      * @param md 元数据
      * @param columnName 列名
-     * @return Map<Integer, Float>，此返回值只能是一行，key为语义编号，float是相似度
+     * @return Map<Integer, Float>，此返回值只能是一行，key为列语义编号，float是相似度
      */
     public Map<Integer, Float> scanOneField(String tableName, MetadataModel md, String columnName);
 }
