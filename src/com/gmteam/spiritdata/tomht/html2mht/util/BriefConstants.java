93efa018f061bac2a1477ddccfe58fa59885a1f1
mahaitao <354650951@qq.com>
2014-11-12 18:55:07 +0800

    Signed-off-by: mahaitao <354650951@qq.com>

--- src/com/gmteam/spiritdata/tomht/html2mht/util/BriefConstants.java
+++ src/com/gmteam/spiritdata/tomht/html2mht/util/BriefConstants.java
@@ -6,6 +6,7 @@ package com.gmteam.spiritdata.tomht.html2mht.util;
  */
 public class BriefConstants {
     //-缓存常量------------------------------------------------------------------------
+    public final static String CATCH="Brief_Store";//模块及系统权限
     public final static String CATCH_BRIEF_STORE="Brief_Store";//模块及系统权限
     public final static String CATCH_BRIEF_ANALYSIS="Brief_Analysis";//模块及系统权限
     public final static String MHT_PATH="html2mht/mhtFolder/";//生成的mht文件目录
