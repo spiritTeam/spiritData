5c17695a0b33f250803a10ef7f88b7bf09b1fa6d
wanghuiwhwx <wanghui_whwx@sina.com>
2014-12-09 14:49:19 +0800

    文件管理

--- src/com/gmteam/spiritdata/filemanage/LOG/model/LogFile.java
+++ src/com/gmteam/spiritdata/filemanage/LOG/model/LogFile.java
@@ -4,16 +4,17 @@ import com.gmteam.framework.core.model.BaseObject;
 
 /**
  * 日志信息，日志统一放在一个目录下(可能是分布式存储的一个根)
- * 日志信息通过log4j实现，是顺序记录的信息，其中的信息有两类：
- * 1-无结构的描述信息，如一般的log日志，包括exception中的类链条
- * 2-有结构的信息，结构为json
- * 如：
- * <prep>
- * 2014-12-09 09:46:44,749 DEBUG <D> 描述信心
- * </prep>
+ * 日志信息通过log4j实现，是顺序记录的信息，其中的信息有两类：<br/>
+ * 1-无结构的描述信息，如一般的log日志，包括exception中的类链条<br/>
+ * 2-有结构的信息，结构为json<br/>
+ * 如：<br/>
+ * <code>
+ * 2014-12-09 09:46:44,749 DEBUG [D] 描述信息
+ * 2014-12-09 09:46:44,749 DEBUG [J] {abc:"", list:[]}
+ * </code>
  * @author wh
  */
 public class LogFile extends BaseObject {
     private static final long serialVersionUID = 956357457822723413L;
-
+    
 }
