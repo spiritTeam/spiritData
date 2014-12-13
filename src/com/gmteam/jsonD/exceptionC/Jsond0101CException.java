e7ce84fac2b797102e496577f60c83e5d0870f4b
wanghuiwhwx <wanghui_whwx@sina.com>
2014-12-13 12:42:36 +0800

    修改了底层包改成0.0.2.3 又加了几个带码异常

--- src/com/gmteam/jsonD/exceptionC/Jsond0101CException.java
+++ src/com/gmteam/jsonD/exceptionC/Jsond0101CException.java
@@ -3,18 +3,19 @@ package com.gmteam.jsonD.exceptionC;
 import com.gmteam.framework.exceptionC.Plat0000CException;
 
 /**
- * JsonD中对象向Json串转换异常，内部码为0101，基本信息为'json转换'
+ * 对象向Json串转换异常，内部码为0001，基本信息为'对象转换为json串'
  * 请参看:
  * {@linkplain com.gmteam.jsonD.util.JsonUtils JsonUtils}
  * @author wh
  */
 public class Jsond0101CException extends JsondCException {
     private static final long serialVersionUID = 1197542690339017535L;
-    private static String myBaseMsg = "json转换";
+
+    private static String myBaseMsg = "对象转换为json串";
     private static int myCode = 101;
 
     /**
-     * 构造没有详细消息内容的——'json转换'异常
+     * 构造没有详细消息内容的——'对象转换为json串'异常
      * @throws Plat0000CException 若设置的分类码或内部码不符合规范
      */
     public Jsond0101CException() throws Plat0000CException {
@@ -22,7 +23,7 @@ public class Jsond0101CException extends JsondCException {
     }
 
     /**
-     * 构造有详细消息内容的——'json转换'异常
+     * 构造有详细消息内容的——'对象转换为json串'异常
      * @param message 详细消息
      * @throws Plat0000CException 若设置的分类码或内部码不符合规范
      */
@@ -31,7 +32,7 @@ public class Jsond0101CException extends JsondCException {
     }
 
     /**
-     * 根据指定的原因和(cause==null?null:cause.toString())的详细消息构造新——'json转换'异常
+     * 根据指定的原因和(cause==null?null:cause.toString())的详细消息构造新——'对象转换为json串'异常
      * @param cause 异常原因，以后通过Throwable.getCause()方法获取它。允许使用null值，指出原因不存在或者是未知的异常
      * @throws Plat0000CException 若设置的分类码或内部码不符合规范
      */
@@ -40,7 +41,7 @@ public class Jsond0101CException extends JsondCException {
     }
 
     /**
-     * 根据指定的原因和(cause==null?null:cause.toString())的详细消息，以及详细消息构造新——'json转换'异常
+     * 根据指定的原因和(cause==null?null:cause.toString())的详细消息，以及详细消息构造新——'对象转换为json串'异常
      * @param message 详细消息
      * @param cause 异常原因，以后通过Throwable.getCause()方法获取它。允许使用null值，指出原因不存在或者是未知的异常
      * @throws Plat0000CException 若设置的分类码或内部码不符合规范
