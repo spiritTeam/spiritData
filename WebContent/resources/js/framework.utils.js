9de2eea4876cd55cb4ff9891fd708abf7d87b3f6
wanghuiwhwx <wanghui_whwx@sina.com>
2014-12-27 11:15:49 +0800

    简单窗口完成了第一个版本。 后续版本最好还有： 1-调整窗口大小+resize事件响应 2-加在iframe的提示
    另外对前台框架进行了调整，这个调整还不是很令人满意

--- WebContent/resources/js/framework.utils.js
+++ WebContent/resources/js/framework.utils.js
@@ -17,6 +17,17 @@ function getMainPage() {
   return mainPage;
 }
 
+//对DOM对象的处理
+/**
+ * 找到顶层页面
+ */
+function getTopWin() {
+  var topWin = window.top;
+  while (topWin.openner) topWin=topWin.operner.top;
+  return topWin;
+}
+
+//===========windows 处理
 /**
  * 创建并打开easyUi窗口
  * @param winOption是一个js对象，目前支持如下参数
@@ -30,75 +41,71 @@ function getMainPage() {
  * winOption.expandAttr 窗口的扩展属性，可定义iframe的id，是javaScript对象，如expandAttr={"frameID":"iframeID"}
  * @returns 返回生成窗口的UUID
  */
-function openWin(winOption) {
+function openWinInMain(winOption) {
   var mainPage = getMainPage();
   if (mainPage) return mainPage.newWin(winOption);//在框架主界面中打开easyUi窗口
   else return newWin(winOption);//在本界面中打开easyUi窗口
 }
 
 /**
- * 创建并打开简单模态窗口
- * @param winOption是一个js对象，目前支持如下参数
- * winOption.title 窗口标题
- * winOption.url 窗口内嵌的iframe的url
- * winOption.height 窗口高度
- * winOption.width 窗口宽度
- * winOption.minButton 最小化窗口按钮是否显示
- * winOption.expandAttr 窗口的扩展属性，可定义iframe的id，是javaScript对象，如expandAttr={"frameID":"iframeID"}
- * @returns 返回生成窗口的UUID
- */
-function openSWin(winOption) {
-  var mainPage = getMainPage();
-  if (mainPage) return mainPage.newSWin(winOption);//在框架主界面中打开简单模态窗口
-  else return newSWin(winOption);//在本界面中打开简单模态窗口
-}
-
-/**
  * 根据winId，得到easyUi窗口
  * @param winId 窗口的ID
  */
-function getWin(winId) {
+function getWinInMain(winId) {
   var mainPage = getMainPage();
   if (mainPage) mainPage.getWin(winId);
   else getWin(winId);
 }
 
 /**
- * 根据winId，得到简单模态
- * @param winId 窗口的ID
- */
-function getSWin(winId) {
-  var mainPage = getMainPage();
-  if (mainPage) mainPage.getSWin(winId);
-  else getSWin(winId);
-}
-
-/**
  * 关闭并销毁easyUi窗口
  * @param winId 窗口的ID
  */
-function closeWin(winId) {
+function closeWinInMain(winId) {
   var mainPage = getMainPage();
   if (mainPage) mainPage.closeWin(winId);
   else closeWin(winId);
 }
 
 /**
+ * 创建并打开简单模态窗口，注意这里总是模态窗口
+ * @param winOption是一个js对象，目前支持如下参数
+ * winOption.height 窗口高度
+ * winOption.width 窗口宽度
+ * winOption.url 窗口内嵌的iframe的url
+ * winOption.content 窗口内的html内容，其与url属性是互斥的，但url的优先级更高
+
+ * winOption.headCss 窗口头样式：主要是高度和背景色
+ * winOption.title 窗口标题
+ * winOption.titleCss 标题样式
+ * winOption.iconCss 窗口图标的css
+ * winOption.iconUrl 窗口图标的url
+
+ * winOption.expandAttr 窗口的扩展属性，可定义iframe的id，是javaScript对象，如expandAttr={"frameID":"iframeID"}
+ * @returns 返回生成窗口的UUID
+ */
+function openSWinInMain(winOption) {
+  var mainPage = getMainPage();
+  if (mainPage) return mainPage.newSWin(winOption);//在框架主界面中打开简单模态窗口
+  else return newSWin(winOption);//在本界面中打开简单模态窗口
+}
+
+/**
+ * 根据winId，得到简单模态
+ * @param winId 窗口的ID
+ */
+function getSWinInMain(winId) {
+  var mainPage = getMainPage();
+  if (mainPage) mainPage.getSWin(winId);
+  else getSWin(winId);
+}
+
+/**
  * 关闭并销毁简单模态
  * @param winId 窗口的ID
  */
-function closeSWin(winId) {
+function closeSWinInMain(winId) {
   var mainPage = getMainPage();
   if (mainPage) mainPage.closeSWin(winId);
   else closeSWin(winId);
-}
-
-//对DOM对象的处理
-/**
- * 找到顶层页面
- */
-function getTopWin() {
-  var topWin = window.top;
-  while (topWin.openner) topWin=topWin.operner.top;
-  return topWin;
 }
