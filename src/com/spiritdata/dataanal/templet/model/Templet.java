c1db292f834c2c3bc56b3b93712f0d96c6269b62
wanghuiwhwx <wanghui_whwx@sina.com>
2015-01-17 10:04:03 +0800

    修改templet的实体定义

--- src/com/spiritdata/dataanal/templet/model/Templet.java
+++ src/com/spiritdata/dataanal/templet/model/Templet.java
@@ -1,7 +1,10 @@
 package com.spiritdata.dataanal.templet.model;
 
+import java.util.List;
+
 import com.spiritdata.dataanal.exceptionC.Dtal1002CException;
 import com.spiritdata.framework.core.model.BaseObject;
+import com.spiritdata.jsonD.model.AccessJsondOne;
 
 /**
  * 模板类，此模板类只包含模板本身的信息，不包括处理模板相关的信息
@@ -12,7 +15,7 @@ public class Templet extends BaseObject {
     private static final long serialVersionUID = 518670183146944686L;
 
     private Object _HEAD;//头信息，可以是String templetHead 对象
-    private List<Ac> _DATA;//数据信息，可以是String templetHead 对象
+    private List<AccessJsondOne> _DATA;//数据信息，可以是String templetHead 对象
     private Object _TEMPLET;//模板主题信息，可以是String templetHead 对象
 
     public Object get_HEAD() {
@@ -20,12 +23,6 @@ public class Templet extends BaseObject {
     }
     public void set_HEAD(Object _HEAD) {
         this._HEAD = _HEAD;
-    }
-    public Object get_DATA() {
-        return _DATA;
-    }
-    public void set_DATA(Object _DATA) {
-        this._DATA = _DATA;
     }
     public Object get_TEMPLET() {
         return _TEMPLET;
@@ -36,7 +33,6 @@ public class Templet extends BaseObject {
 
     public String convert2Json() {
         if (_HEAD==null) throw new Dtal1002CException("templetD不规范：头信息(_HEAD)必须设置！");
-        if (_DATA==null) throw new Dtal1002CException("templetD不规范：数据信息(_DATA)必须设置！");
         if (_TEMPLET==null) throw new Dtal1002CException("templetD不规范：模板主题信息(_TEMPLET)必须设置！");
         
         return null;
