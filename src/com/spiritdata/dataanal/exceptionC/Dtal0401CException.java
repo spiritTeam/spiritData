18fe5b554e550f3e51d543b6468f454e5687428d
wanghuiwhwx <wanghui_whwx@sina.com>
2015-03-05 17:43:51 +0800

    修改文档，修改代码异常

--- src/com/spiritdata/dataanal/exceptionC/Dtal0401CException.java
+++ src/com/spiritdata/dataanal/exceptionC/Dtal0401CException.java
@@ -1,20 +1,19 @@
-package com.spiritdata.jsonD.exceptionC;
+package com.spiritdata.dataanal.exceptionC;
+
 /**
- * Task转换为JsonD的access对象异常，内部码为0102，基本信息为'Task转换为JsonD的access'
- * 请参看:
- * {@linkplain com.spiritdata.jsonD.util.JsonUtils JsonUtils}
+ * 任务数据转换为jsonD，内部码为0401，基本信息为'Task转换为JsonD的access'异常
  * @author wh
  */
-public class JsonD0103CException extends JsonDCException {
-    private static final long serialVersionUID = -7562893262042593302L;
+public class Dtal0401CException extends DtalCException {
+    private static final long serialVersionUID = -5107800087154965799L;
 
     private static String myBaseMsg = "Task转换为JsonD的access";
-    private static int myCode = 103;
+    private static int myCode = 301;
 
     /**
      * 构造没有详细消息内容的——'Task转换为JsonD的access'异常
      */
-    public JsonD0103CException() {
+    public Dtal0401CException() {
         super(myCode, myBaseMsg);
     }
 
@@ -22,7 +21,7 @@ public class JsonD0103CException extends JsonDCException {
      * 构造有详细消息内容的——'Task转换为JsonD的access'异常
      * @param message 详细消息
      */
-    public JsonD0103CException(String msg) {
+    public Dtal0401CException(String msg) {
         super(myCode, myBaseMsg, msg);
     }
 
@@ -30,7 +29,7 @@ public class JsonD0103CException extends JsonDCException {
      * 根据指定的原因和(cause==null?null:cause.toString())的详细消息构造新——'Task转换为JsonD的access'异常
      * @param cause 异常原因，以后通过Throwable.getCause()方法获取它。允许使用null值，指出原因不存在或者是未知的异常
      */
-    public JsonD0103CException(Throwable cause) {
+    public Dtal0401CException(Throwable cause) {
         super(myCode, myBaseMsg, cause);
     }
 
@@ -39,11 +38,11 @@ public class JsonD0103CException extends JsonDCException {
      * @param message 详细消息
      * @param cause 异常原因，以后通过Throwable.getCause()方法获取它。允许使用null值，指出原因不存在或者是未知的异常
      */
-    public JsonD0103CException(String msg, Throwable cause) {
+    public Dtal0401CException(String msg, Throwable cause) {
         super(myCode, myBaseMsg, msg, cause);
     }
 
-    public JsonD0103CException(String msg, Throwable cause, boolean enableSuppression,
+    public Dtal0401CException(String msg, Throwable cause, boolean enableSuppression,
             boolean writableStackTrace) {
         super(myCode, myBaseMsg, msg, cause, enableSuppression, writableStackTrace);
     }
