77314c8dd9936cdbdb0cf9d67581ae5ccc2a52b9
yuanfeng <yuanfeng@10.1.0.46>
2015-04-07 11:22:23 +0800

    加入了X、Y常量的定义

--- src/com/spiritdata/dataanal/metadata/relation/semanteme/SemantemeType.java
+++ src/com/spiritdata/dataanal/metadata/relation/semanteme/SemantemeType.java
@@ -3,4 +3,6 @@ package com.spiritdata.dataanal.metadata.relation.semanteme;
 public class SemantemeType {
      public static int DICT=1; //字典类型
      public static int PERSON_IDENTIFY=2;//身份证
+     public static int COORD_X = 3;//X坐标 
+     public static int COORD_Y = 4;//Y坐标
 }
