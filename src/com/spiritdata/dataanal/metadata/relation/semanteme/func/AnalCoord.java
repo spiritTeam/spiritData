89756401b611978857079a1a55b202bbf39b4970
yfo <yfo@10.1.0.22>
2015-04-08 16:32:05 +0800

    加入了坐标列分析结果写JSON文件

--- src/com/spiritdata/dataanal/metadata/relation/semanteme/func/AnalCoord.java
+++ src/com/spiritdata/dataanal/metadata/relation/semanteme/func/AnalCoord.java
@@ -1,7 +1,10 @@
 package com.spiritdata.dataanal.metadata.relation.semanteme.func;
 
+import java.io.File;
 import java.util.ArrayList;
+import java.util.Date;
 import java.util.HashMap;
+import java.util.LinkedHashMap;
 import java.util.List;
 import java.util.Map;
 
@@ -10,6 +13,7 @@ import javax.annotation.Resource;
 import org.apache.poi.util.StringUtil;
 import org.springframework.stereotype.Component;
 
+import com.spiritdata.dataanal.SDConstants;
 import com.spiritdata.dataanal.exceptionC.Dtal0203CException;
 import com.spiritdata.dataanal.exceptionC.DtalCException;
 import com.spiritdata.dataanal.metadata.relation.pojo.MetadataModel;
@@ -19,29 +23,44 @@ import com.spiritdata.dataanal.metadata.relation.semanteme.AnalMetadata;
 import com.spiritdata.dataanal.metadata.relation.semanteme.func.coord.pojo.CoordRegularBean;
 import com.spiritdata.dataanal.metadata.relation.semanteme.func.coord.pojo.SimilarResultBean;
 import com.spiritdata.dataanal.metadata.relation.service.MdQuotaService;
+import com.spiritdata.filemanage.category.ANAL.model.AnalResultFile;
+import com.spiritdata.filemanage.category.ANAL.service.AanlResultFileService;
 import com.spiritdata.framework.util.JsonUtils;
+import com.spiritdata.framework.util.SequenceUUID;
 import com.spiritdata.framework.util.StringUtils;
 import com.spiritdata.jsonD.model.JsonD;
+import com.spiritdata.jsonD.model.JsonDAtomData;
+import com.spiritdata.jsonD.model.JsonDHead;
 
 /**
- * 分析元数据的坐标信息
- * 
+ * 分析元数据列的坐标信息，记录哪些列可能是X坐标列、哪些列可能是Y坐标列
+ * 每次分析后会生成一个元数据坐标列分析结果文件
+ * 文件存储位置：WebContent/analData/METADATA/coord
+ * 文件命名规则：md_metaTableModelId_当前时间的长整形.json  其中metaTableModelId:SD_MD_TABMODULE 指标表的ID
  * @author yfo
  * 
  */
 @Component
 public class AnalCoord implements AnalMetadata {
+	/**
+	 * 相似度值分析结果通过线值
+	 * 当相似度值高于通过线值时，才会写入元数据列分析结果文件中
+	 */
+	public static final float ANAL_COORD_SIMILAR_PASS_VAL = 0.8f;
+	
 	@Resource
-	private MdQuotaService mdQuotaService;
+	private MdQuotaService mdQuotaService; //元数据指标列读取服务
+    @Resource
+    private AanlResultFileService arfService; //分析结果文件存取服务
 
 	// 中国范围的经度判断参数JSON，可以考虑从配置文件中读取!!!
-	public static final String BAIDU_CHINA_COORDX = "{min:73.508097,max:135.083849,keywords:'x,lon, longitude,jd,jingdu,经度'}";
-	// 中国范围的纬度判断参数JSON
-	public static final String BAIDU_CHINA_COORDY = "{min:18.164892,max:53.569653,keywords:'y,lat, latitude,jd,weidu,维度'}";
+	public static final String BAIDU_CHINA_COORDX = "{min:73.508097,max:135.083849,weightSimilarRangeVal:0.5,keywords:'x,lon,longitude,jd,jingdu,经度',weightSimilarKeyVal:0.5,weightExcludeKeyVal:0}";
+	// 中国范围的纬度判断参数JSON                                                                           
+	public static final String BAIDU_CHINA_COORDY = "{min:18.164892,max:53.569653,weightSimilarRangeVal:0.5,keywords:'y,lat,latitude,wd,weidu,维度',weightSimilarKeyVal:0.5,weightExcludeKeyVal:0}";
 
 	// X坐标关键字
-	public static final String COORD_TYPE_X = "X";
-	public static final String COORD_TYPE_Y = "Y";
+//	public static final String COORD_TYPE_X = "X";
+//	public static final String COORD_TYPE_Y = "Y";
 
 	// X坐标列参数判断BEAN
 	private CoordRegularBean coordXBean;
@@ -93,11 +112,35 @@ public class AnalCoord implements AnalMetadata {
 		        
 		// 存入XY列分析结果
 		Map<String, Object> ret = new HashMap<String, Object>();
-		ret.put(COORD_TYPE_X, colXList);
-		ret.put(COORD_TYPE_Y, colYList);
 
         //组织JsonD，并写入文件
-        JsonD analDictJsonD = new JsonD();
+        JsonD analCoordJsonD = new JsonD();
+        //头
+        JsonDHead jsonDHead = new JsonDHead();
+        jsonDHead.setId(SequenceUUID.getPureUUID());
+        jsonDHead.setCode(SDConstants.JDC_ANAL_COORD);
+        jsonDHead.setCTime(new Date());
+        jsonDHead.setDesc("分析元数据["+mm.getTitleName()+"("+mm.getId()+")]的坐标信息");
+        //数据体
+        Map<String, Object> _DATA_Map = new HashMap<String, Object>();
+        JsonDAtomData _dataElement = new JsonDAtomData("_mdMId", "string", mm.getId());
+        _DATA_Map.putAll(_dataElement.toJsonMap());
+        _DATA_Map.put("_analResults", colsListToJsonMap());
+        //设置JsonD
+        analCoordJsonD.set_HEAD(jsonDHead);
+        analCoordJsonD.set_DATA(_DATA_Map);
+        //分析结果文件种子设置
+        AnalResultFile arfSeed = new AnalResultFile();
+        arfSeed.setAnalType(SDConstants.ANAL_MD_COORD); //分析类型
+        arfSeed.setSubType(mm.getId()); //下级分类
+        arfSeed.setObjType("metadata"); //所分析对象
+        arfSeed.setObjId("["+mm.getTitleName()+"("+mm.getId()+")]"); //所分析对象的ID
+        arfSeed.setFileNameSeed("METADATA"+File.separator+"coord"+File.separator+"md_"+mm.getId()+new Date().getTime());
+        arfSeed.setJsonDCode(SDConstants.JDC_ANAL_COORD);
+
+        AnalResultFile arf = (AnalResultFile)arfService.write2FileAsJson(analCoordJsonD, arfSeed);
+        //回写文件信息到返回值
+        ret.put("resultFile", arf);
         
         
 		return ret;
@@ -186,4 +229,62 @@ public class AnalCoord implements AnalMetadata {
 		return ret;
 	}
 
+	/**
+	 * 将坐标分析结果MAP转换成LIST
+	 * @param coordAnalResultMap
+	 * @return
+	 */
+    private Map<String,List<Map<String,Object>>> colsListToJsonMap() {        
+        Map<String,List<Map<String,Object>>> ret = new LinkedHashMap<String,List<Map<String,Object>>>();
+        
+        //读取X列分析列结果 
+        List<SimilarResultBean> colXPassList = filterPassValcolList(this.colXList,ANAL_COORD_SIMILAR_PASS_VAL);
+        List<Map<String,Object>> colXJsonList = colList2JsonStr(colXPassList);
+        ret.put("xCols", colXJsonList);
+        //读取Y列分析结果
+        List<SimilarResultBean> colYPassList = filterPassValcolList(this.colYList,ANAL_COORD_SIMILAR_PASS_VAL);
+        List<Map<String,Object>> colYJsonList = colList2JsonStr(colYPassList);
+        ret.put("yCols", colYJsonList);
+
+        return ret;
+    }
+    
+    /**
+     * 根据通过值,过滤出相似度值>通过值的列信息
+     * @param colList
+     * @param passVal
+     * @return
+     */
+    private List<SimilarResultBean> filterPassValcolList(List<SimilarResultBean> colList, float passVal){
+    	List<SimilarResultBean> retList = new ArrayList<SimilarResultBean> ();
+    	for(SimilarResultBean asrb : colList){
+    		if(asrb.getSimilarSortVal()>=passVal){
+    			retList.add(asrb);
+    		}else{ //因为之前已经做过了值从大到小的排序，所以如果当前值小于指定的通过值，则其后的值肯定都小
+    			break;
+    		}
+    	}
+    	
+    	return retList;
+    }
+    
+    /**
+     * 将一个colList转换成JSON串
+     * @param colList
+     * @return
+     */
+    private List<Map<String,Object>> colList2JsonStr(List<SimilarResultBean> colList){
+    	List<Map<String,Object>> retList = new ArrayList<Map<String,Object>>();
+    	for(SimilarResultBean asrb : colList){
+    		Map<String, Object> oneMap = new HashMap<String, Object>();
+    		oneMap.put("colName", asrb.getColQuota().getColumn().getTitleName());
+    		oneMap.put("similarSortVal", asrb.getSimilarSortVal());
+    		oneMap.put("similarRangeVal", asrb.getSimilarRangeVal());
+    		oneMap.put("similarKeyVal", asrb.getSimilarKeyVal());
+    		oneMap.put("excludeKeyVal", asrb.getExcludeKeyVal());
+    		retList.add(oneMap);
+    	}
+    	
+    	return retList;
+    }
 }
