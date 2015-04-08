d543967c09a2e110c9907b6ed5e218241c647713
Administrator <Administrator@PC-201412021850>
2015-04-08 20:34:49 +0800

    Signed-off-by: Administrator <Administrator@PC-201412021850>

--- src/com/spiritdata/dataanal/expreport/word/service/WordService.java
+++ src/com/spiritdata/dataanal/expreport/word/service/WordService.java
@@ -1,8 +1,8 @@
 package com.spiritdata.dataanal.expreport.word.service;
 
-import java.io.FileOutputStream;
 import java.io.IOException;
 import java.io.OutputStream;
+import java.lang.reflect.Field;
 import java.util.ArrayList;
 import java.util.Iterator;
 import java.util.List;
@@ -10,14 +10,15 @@ import java.util.Map;
 
 import javax.annotation.Resource;
 
-import org.apache.poi.xwpf.usermodel.XWPFDocument;
-import org.apache.poi.xwpf.usermodel.XWPFParagraph;
-import org.apache.poi.xwpf.usermodel.XWPFRun;
+import org.junit.Test;
 import org.springframework.stereotype.Service;
 
 import com.spiritdata.dataanal.UGA.pojo.User;
+import com.spiritdata.dataanal.expreport.word.WordConstants;
 import com.spiritdata.dataanal.report.model.Report;
+import com.spiritdata.dataanal.report.model.ReportHead;
 import com.spiritdata.dataanal.report.service.ReportService;
+import com.spiritdata.jsonD.model.AccessJsonD;
 import com.spiritdata.jsonD.model.JsonD;
 import com.spiritdata.jsonD.util.JsonUtils;
 import com.spiritdata.jsonD.web.service.JsonDService;
@@ -80,6 +81,7 @@ public class WordService {
         Map<String,Object> reportMap = (Map<String, Object>)JsonUtils.jsonToObj(reportJson, Map.class);
         map2been(reportMap);
         //jsonD
+        System.out.println("1");
         if(this.report!=null){
         }
     }
@@ -88,20 +90,102 @@ public class WordService {
 	 * 
 	 * @param reportMap
 	 * @param class1
+	 * @throws IllegalAccessException 
+	 * @throws IllegalArgumentException 
+	 * @throws ClassNotFoundException 
+	 * @throws InstantiationException 
 	 */
-    private void map2been(Map<String, Object> reportMap) {
-		Iterator<String> it = reportMap.keySet().iterator();
-		while (it.hasNext()) {
-			String key = it.next();
-			Object value = reportMap.get(key);
-			if (value instanceof String) {
-				System.out.println("key="+key+":val="+value);
-			} else {
-				System.out.println("key="+key);
-			}
+    @SuppressWarnings("unchecked")
+	private void map2been(Map<String, Object> reportMap) throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, InstantiationException {
+    	//第一层  dlist，report，head
+    	//_DLIST
+    	List<Map<String,Object>> reportDList = (List<Map<String, Object>>) reportMap.get(WordConstants.REPORT_DLIST);
+    	//_REPORT
+    	List<Map<String,Object>> reportReportList = (List<Map<String, Object>>) reportMap.get(WordConstants.REPORT_REPORT);
+    	//_HEAD
+		Map<String,Object> reportHeadMap = (Map<String, Object>) reportMap.get(WordConstants.REPORT_HEAD);
+		//第二层 
+		//head
+		ReportHead reportHead = (ReportHead) fieldBeen(ReportHead.class,reportHeadMap);
+		//dList
+		List<AccessJsonD> accessJsonDList = new ArrayList<AccessJsonD>();
+		for (Map<String,Object> dListMap :reportDList) {
+			AccessJsonD accessJonnD = (AccessJsonD) fieldBeen(ReportHead.class,dListMap);
+			accessJsonDList.add(accessJonnD);
 		}
+		//segMent
 	}
-
+    /**
+     * @param propertyName 
+     * @param val 
+     * @param rh2 
+     * @throws ClassNotFoundException
+     * @throws IllegalArgumentException
+     * @throws IllegalAccessException
+     * @throws InstantiationException 
+     */
+	public Object fieldBeen(Class<?> cls, Map<String,Object> dataMap) throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, InstantiationException{
+		Iterator<String> headIt = dataMap.keySet().iterator();
+    	Object retObj = null;
+		Field fields[] = cls.getDeclaredFields();
+    	for(Field field :fields){
+    		String fieldName = field.getName();
+    		field.setAccessible(true);
+    		if (cls.getName().equals("com.spiritdata.dataanal.report.model.ReportHead")) {
+    			ReportHead reportHead = (ReportHead) cls.newInstance();
+    			while(headIt.hasNext()){
+    				String propertyName = headIt.next();
+    				Object val = dataMap.get(propertyName);
+    				if(fieldName.equals(propertyName)){
+            			field.set(reportHead, val);
+            		}
+    			}
+    			retObj = reportHead;
+        	} else if(cls.getName().equals("com.spiritdata.jsonD.model.AccessJsonD")) {
+        		AccessJsonD accessJsonD = (AccessJsonD) cls.newInstance();
+        		while(headIt.hasNext()){
+    				String propertyName = headIt.next();
+    				Object val = dataMap.get(propertyName);
+    				if(fieldName.equals(propertyName)){
+            			field.set(accessJsonD, val);
+            			if(fieldName.equals(propertyName)){
+                			field.set(accessJsonD, val);
+                		}
+            		}
+    			}
+        		retObj = accessJsonD;
+        	}
+    	}
+		return retObj;
+	}
+	public AccessJsonD fieldBeen(AccessJsonD accessJonnD, String propertyName, Object val) throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException{
+    	Class<?> c=Class.forName(accessJonnD.getClass().getName());
+    	Field fields[]=c.getDeclaredFields();
+    	for(Field field :fields){
+    		String fieldName = field.getName();
+    		field.setAccessible(true);
+    		if(fieldName.equals(propertyName)){
+    			field.set(accessJonnD, val);
+    		}
+    	}
+		return accessJonnD;
+	}
+	@Test
+	public void fieldBeen() throws ClassNotFoundException, IllegalArgumentException, IllegalAccessException, InstantiationException{
+    	Class<?> c=Class.forName(ReportHead.class.getName());
+    	System.out.println(c.getName());
+    	if(ReportHead.class.getName().equals("com.spiritdata.dataanal.report.model.ReportHead")){
+    		ReportHead c1 =  ReportHead.class.newInstance();
+        	Field fields[]=c.getDeclaredFields();
+        	for(Field field :fields){
+        		String fieldName = field.getName();
+        		field.setAccessible(true);
+        		if(fieldName.equals("id")){
+        			field.set(c1, "123");
+        		}
+        	}
+    	}
+	}
 	/**
      * 创建word
      * @return 
