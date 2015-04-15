50d2a3beb12345ca68b221410628e29fa248e6f8
yfo <yfo@10.1.0.154>
2015-04-15 18:52:44 +0800

    Signed-off-by: yfo <yfo@10.1.0.154>

--- src/com/spiritdata/dataanal/metadata/relation/semanteme/SemantemeType.java
+++ src/com/spiritdata/dataanal/metadata/relation/semanteme/SemantemeType.java
@@ -4,6 +4,6 @@ public class SemantemeType {
      public static int DICT=1; //字典类型
      public static int PERSON_IDENTIFY=2;//身份证
      public static int COORD_X = 3;//X坐标 
-     public static int COORD_Y = 4;//Y坐标   sssdddd
+     public static int COORD_Y = 4;//Y坐标
      
 }
